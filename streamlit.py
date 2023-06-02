c4fa2b4ccb5aa7a62e04db8187c2f1e3903878df
Raghav Ravishankar <113712354+alyosha-swamy@users.noreply.github.com>
2023-06-02 16:01:37 +0530

    pushw

--- streamlit.py
+++ streamlit.py
@@ -1,77 +1,58 @@
 import json
 import requests
 import streamlit as st
+import toml
+from tempfile import _TemporaryFileWrapper
+
 config = toml.load("config.toml")
 
+def ask_api(lcserve_host: str, url: str, file: _TemporaryFileWrapper, question: str, claude_key: str) -> str:
+    if not lcserve_host.startswith('http'):
+        return '[ERROR]: Invalid API Host'
 
+    if url.strip() == '' and file is None:
+        return '[ERROR]: Both URL and PDF are empty. Provide at least one.'
 
-def ask_api(
- lcserve_host: str,
- url: str,
- file: _TemporaryFileWrapper,
- question: str,
- claude_key: str,
-) -> str:
- if not lcserve_host.startswith('http'):
- return '[ERROR]: Invalid API Host'
+    if url.strip() != '' and file is not None:
+        return '[ERROR]: Both URL and PDF are provided. Please provide only one (either URL or PDF).'
 
- if url.strip() == '' and file == None:
- return '[ERROR]: Both URL and PDF is empty. Provide atleast one.'
+    if question.strip() == '':
+        return '[ERROR]: Question field is empty'
 
- if url.strip() != '' and file != None:
- return '[ERROR]: Both URL and PDF is provided. Please provide only one (eiter URL or PDF).'
+    _data = {
+        'question': question,
+        'envs': {
+            'CLAUD_API_KEY': claude_key,
+        },
+    }
 
- if question.strip() == '':
- return '[ERROR]: Question field is empty'
+    if url.strip() != '':
+        r = requests.post(f'{lcserve_host}/ask_url', json={'url': url, **_data})
+    else:
+        with open(file.name, 'rb') as f:
+            r = requests.post(f'{lcserve_host}/ask_file', params={'input_data': json.dumps(_data)}, files={'file': f})
 
- _data = {
- 'question': question,
- 'envs': {
- 'CLAUD_API_KEY': claude_key,
- },
- }
+    if r.status_code != 200:
+        raise ValueError(f'[ERROR]: {r.text}')
 
- if url.strip() != '':
- r = requests.post(
- f'{lcserve_host}/ask_url',
- json={'url': url, **_data},
- )
+    return r.json()['result']
 
- else:
- with open(file.name, 'rb') as f:
- r = requests.post(
- f'{lcserve_host}/ask_file',
- params={'input_data': json.dumps(_data)},
- files={'file': f},
- )
-
- if r.status_code != 200:
- raise ValueError(f'[ERROR]: {r.text}')
-
- return r.json()['result']
 
 title = 'PDF Claude'
-description = """ PDF Claude allows you to chat with your PDF file using Universal Sentence Encoder and Claude. It gives hallucination free response than other tools as the embeddings are better than OpenAI. The returned response can even cite the page number in square brackets([]) where the information is located, adding credibility to the responses and helping to locate pertinent information quickly."""
+description = """PDF Claude allows you to chat with your PDF file using Universal Sentence Encoder and Claude. It gives hallucination-free responses compared to other tools as the embeddings are better than OpenAI. The returned response can even cite the page number in square brackets ([]) where the information is located, adding credibility to the responses and helping to locate pertinent information quickly."""
 
 st.title(title)
 st.markdown(description)
 
-lcserve_host = st.text_input(
- label='Enter your API Host here',
- value='http://localhost:8080',
-)
-st.markdown(
- f'<p style="text-align:center">Get your Claude API key <a href="https://console.anthropic.com/account/api-keys">here</a></p>', unsafe_allow_html=True
-)
+lcserve_host = st.text_input(label='Enter your API Host here', value='http://localhost:8080')
+st.markdown(f'<p style="text-align:center">Get your Claude API key <a href="https://console.anthropic.com/account/api-keys">here</a></p>', unsafe_allow_html=True)
 claude_key = config["claude"]["api_key"]
 pdf_url = st.text_input(label='Enter PDF URL here')
 st.markdown("<center><h4>OR<h4></center>", unsafe_allow_html=True)
-file = st.file_uploader(
- label='Upload your PDF/ Research Paper / Book here', type=['pdf']
-)
+file = st.file_uploader(label='Upload your PDF/Research Paper/Book here', type=['pdf'])
 question = st.text_input(label='Enter your question here')
 btn = st.button(value='Submit')
 
 if btn:
- answer = ask_api(lcserve_host, pdf_url, file, question, claude_key)
- st.text_area(label='The answer to your question is :', value=answer)
+    answer = ask_api(lcserve_host, pdf_url, file, question, claude_key)
+    st.text_area(label='The answer to your question is:', value=answer)
