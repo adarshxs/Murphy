8d08edbf5a318171b4c26a2e1891835cfbd4f417
Adarsh Shirawalmath <114558126+adarshxs@users.noreply.github.com>
2023-06-04 00:04:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ from streamlit_chat import message
 
 
 st.set_page_config(page_title="Murph", page_icon="📖")
+st.title("Bhagwaan Bharose :sunglasses:")
+st.image("logo.png", use_column_width=True)
 st.write("Welcome to my multi-page app! Use the sidebar to navigate between pages.")
 st.write("![Your Awsome GIF](https://media.giphy.com/media/3ohzdIuqJoo8QdKlnW/giphy.gif)")
 def PDF():
